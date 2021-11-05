93e2ec9a32ddc298e194307611d0c98002f5017e
HiddyTiddy <hiddy.tiddey@gmail.com>
2021-11-05 13:09:04 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,124 @@
+extern crate kiss3d;
+extern crate nalgebra as na; // kinda cool https://www.nalgebra.org/
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use kiss3d::resource::Mesh;
+use kiss3d::light::Light;
+use kiss3d::window::Window;
+use na::{Point3, Vector3};
+
+const LEN: i32 = 50;
+const RADIUS : f32 = 1.0f32;
+mod map;
+
+trait Normaliz {
+    fn normalize(&self) -> Point3<f32>;
+    fn scalar_mult(&self, scalar: f32) -> Point3<f32>;
+}
+
+impl Normaliz for Point3<f32> {
+    fn normalize(&self) -> Point3<f32> {
+        // let abs: f32 = self.x * self.x + self.y * self.y + self.z * self.z;
+        // let abs = abs.sqrt() / 10.;
+        // Point3::new(self.x / abs, self.y / abs, self.z / abs)
+
+        let x2: f32 = self.x * self.x;
+        let y2: f32 = self.y * self.y;
+        let z2: f32 = self.z * self.z;
+
+        // let x = self.x * (1. - (y2 / 2.) - (z2 / 2.) + (y2 * z2 / 3.)).sqrt();
+        // let y = self.y * (1. - (z2 / 2.) - (x2 / 2.) + (x2 * z2 / 3.)).sqrt();
+        // let z = self.z * (1. - (x2 / 2.) - (y2 / 2.) + (x2 * y2 / 3.)).sqrt();
+        // let x = self.x / (x2+y2+z2).sqrt();
+        // let y = self.y / (x2+y2+z2).sqrt();
+        // let z = self.z / (x2+y2+z2).sqrt();
+        let x:f32 = self.x * ((1.0 - (y2 + z2) / 2.0 + (y2 * z2) / 3.).sqrt());
+        let y:f32 = self.y * ((1.0 - (z2 + x2) / 2.0 + (x2 * z2) / 3.).sqrt());
+        let z:f32 = self.z * ((1.0 - (x2 + y2) / 2.0 + (x2 * y2) / 3.).sqrt());
+        // println!("({}, {}, {}) -> {}, {}, {}",self.x, self.y, self.z, x,y,z);
+        Point3::new(x, y, z)
+    }
+
+    fn scalar_mult(&self, s: f32) -> Point3<f32> {
+        Point3::new(self.x * s, self.y * s, self.z * s)
+    }
+}
+
+fn make_point(i: f32, j: f32, side: u8, karte: &map::Map) -> Point3<f32> {
+    let out = match side {
+        0 => Point3::new(i, j, RADIUS),  // front
+        1 => Point3::new(i, j, -RADIUS), // back
+        2 => Point3::new(i, RADIUS, j),
+        3 => Point3::new(i, -RADIUS, j),
+        4 => Point3::new(RADIUS, i, j),
+        5 => Point3::new(-RADIUS, i, j),
+        _ => unreachable!(),
+    }.normalize();
+    //let height = karte.height_at_point(out);
+    out//.scalar_mult(height)
+}
+
+fn gen_mesh(karte: &map::Map) -> Rc<RefCell<Mesh>> {
+    let mut vertices = vec![];
+    let mut indices = vec![];
+    let c = (2. * RADIUS) / (LEN as f32);
+    let mut index = 0;
+    for side in 0..6u8 {
+        for i in 0..LEN {
+            for j in 0..LEN {
+                let x: f32 = j as f32 * c - 1.;
+                let y: f32 = i as f32 * c - 1.;
+                // not optimal 
+                // redeclare like most of the vertices but it's fine
+                vertices.push(make_point(x, y, side, karte));
+                vertices.push(make_point(x + c, y, side, karte));
+                vertices.push(make_point(x, y + c, side, karte));
+                vertices.push(make_point(x + c, y + c, side, karte));
+                indices.push(Point3::new(
+                    index as u16,
+                    (index + 1) as u16,
+                    (index + 2) as u16,
+                ));
+                indices.push(Point3::new(
+                    (index + 1) as u16,
+                    (index + 2) as u16,
+                    (index + 3) as u16,
+                ));
+                index += 4;
+            }
+        }
+    }
+    // println!("{:?} {:?}", indices.len(), vertices.len());
+
+    Rc::new(RefCell::new(Mesh::new(
+        vertices, indices, None, None, false,
+    )))
+}
+
+fn main() {
+    let karte = map::Map::new("data/earth-heightmap.png");
+    
+    let mut window = Window::new("yay");
+    let mesh = gen_mesh(&karte);
+
+    // // let mesh = Rc::new(RefCell::new(Mesh::new(
+    // //     vert, indices, None, None, false
+    // // )));
+
+    let mut c = window.add_mesh(mesh, Vector3::new(1.0, 1.0, 1.0));
+    c.set_color(0.8, 0.8, 0.8);
+    c.enable_backface_culling(false);
+
+    window.set_light(Light::StickToCamera);
+    // println!("hi");
+
+    while window.render() {
+        // c.prepend_to_local_rotation(&rot);
+    }
+    
+
+    
+    // read_png("data/earth-heightmap.png");
+}
---
+++ src/map.rs
@@ -0,0 +1,151 @@
+use ang::atan2;
+
+use crate::Point3;
+use std::{f32::consts::PI, fs::File};
+
+pub struct Map {
+    buffer: Vec<u8>,
+    height: u32,
+    width: u32,
+}
+
+const FACTOR: f32 = 1.0 / 255.0;
+
+impl Map {
+    /*
+     * creates new Map struct from grayscale image
+     * i think this works fine
+     */
+    pub fn new(filename: &str) -> Map {
+        let decoder = png::Decoder::new(File::open(filename).unwrap());
+        let mut reader = decoder.read_info().unwrap();
+        let mut buf = vec![0; reader.output_buffer_size()];
+        let info = reader.next_frame(&mut buf).unwrap();
+        let bytes = &buf[..info.buffer_size()];
+        Map {
+            buffer: Vec::from(bytes),
+            height: info.height,
+            width: info.width,
+        }
+    }
+
+    /*
+     * helper function to index image
+     */
+    fn at(&self, x: u32, y: u32) -> u8 {
+        let idx = (x + self.width * y) as usize;
+        assert!(x < self.width && y < self.height);
+        if idx as usize >= self.buffer.len() {
+            panic!("out of bounds with x={} and y={}", x, y);
+        } else {
+            self.buffer[idx]
+        }
+    }
+
+    /*
+     * returns height as given by color value of the image at the specified point.
+     * this does not yet work properly
+     */
+    pub fn height_at_point(&self, point: Point3<f32>) -> f32 {
+        let (x, y) = self.point_to_coordinate(point);
+        // println!("{} {}", lat, lon);
+
+        let val = self.at(x, y);
+
+        println!( "{} {} {} -> {} {} -> {}",
+            point.x,
+            point.y,
+            point.z,
+            x,
+            y,
+            if val == 0xff {
+                1.0
+            } else {
+                1.0 + (val as f32) * FACTOR
+            }
+        ); // dump
+
+           // println!("{} {} {:?}", x,y, rgb);
+
+        if val == 0xff {
+            1.0
+        } else {
+            //1.0 + (val as f32) * FACTOR
+            1.1
+        }
+    }
+
+    // fn dumb_point_to_coordinate(&self, point: Point3<f32>) -> (u32, u32) {
+    //     let lat = atan2(point.y, point.x).in_radians(); // -pi/2 to pi/2
+    //     let lon: f32 = if point.x != 0.0 {
+    //         (point.z / point.x).atan()
+    //     } else if point.z > 0. {
+    //             PI / 2.
+    //         } else {
+    //             -PI / 2.
+    //     }; // -pi to pi
+    //        // let y = lat.tan().
+
+    //     let x:f32 = lat / PI +0.5;
+    //     let y:f32 = lon / PI / 2.0 + 0.5;
+
+    //     ((x * self.width as f32) as u32, (y * self.height as f32) as u32)
+    // }
+
+    fn point_to_coordinate(&self, point: Point3<f32>) -> (u32, u32) {
+        // let lat = point.z.acos(); // -pi/2 to pi/2
+        // let lon: f32 = ang::atan2(point.x, point.y).in_radians();
+        // let y = lat.tan().
+
+        let (lat, lon) = point_to_coord(point);
+        // println!("{} {}", lat, lon);
+
+        let y: f32 = (lon + PI) / 2.0 / PI; //((lat + PI) % (2.*PI)) / PI * 0.5;
+        let x: f32 = dumb_y(lat);
+        // println!("{} <> {}", lat, y);
+        //   let y = y / 3.800_201_2 / 2.0 + 0.5;
+
+        (
+            (x * self.width as f32) as u32,
+            (y * self.height as f32) as u32,
+        )
+    }
+}
+
+const LIMIT: f32 = 1.526_071_1;
+
+fn mercator_y(latitude: f32) -> f32 {
+    // if latitude > LIMIT {
+    //     latitude = LIMIT - 0.0001;
+    // } else if latitude < -LIMIT {
+    //     latitude = -LIMIT + 0.0001;
+    // }
+    // let tan = (latitude / 2.0 + PI / 4.).tan();
+    // tan.ln() /  / 2.0 + 0.5
+    if !(-LIMIT..=LIMIT).contains(&latitude) {
+        latitude / PI
+    } else {
+        let val = (latitude / 2.0 + PI / 4.).tan().ln();
+        val / (LIMIT / 2.0 + PI / 4.0).tan().ln() * 0.4 + 0.5
+    }
+}
+
+fn dumb_y(latitude: f32) -> f32 {
+    latitude / PI 
+}
+
+
+// yanked from https://www.youtube.com/watch?v=sLqXFF8mlEU
+fn point_to_coord(point: Point3<f32>) -> (f32, f32) {
+    let longitude = point.y.asin();
+    let latitude = atan2(point.x,point.z).in_radians();
+    (latitude, longitude)
+}
+
+// fn coordinate_to_point(lat: f32, lon: f32) -> Point3<f32>{
+//     let y = lat.sin();
+//     let r = lat.cos();
+//     let x = lon.sin() * r;
+//     let z = -lon.cos() * r;
+//     Point3::new(x,y,z)
+// }
