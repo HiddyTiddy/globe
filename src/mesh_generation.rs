c4c563a7c2569a4bdefdeccb30ff6128b48b1516
HiddyTiddy <hiddy.tiddey@gmail.com>
2021-11-10 09:43:01 +0100

    where >>>

--- src/mesh_generation.rs
+++ src/mesh_generation.rs
@@ -4,17 +4,17 @@ use crate::constants::{LEN, RADIUS};
 use kiss3d::resource::Mesh;
 use nalgebra::Point3;
 
+/*
+dumb trait for Point3<f32>
+*/
 trait Normalize {
-    fn normalize(&self) -> Point3<f32>;
-    fn scalar_mult(&self, scalar: f32) -> Point3<f32>;
+    fn normalize(&self) -> Self;
+    fn scalar_mult(&self, scalar: f32) -> Self;
 }
 
 impl Normalize for Point3<f32> {
     fn normalize(&self) -> Point3<f32> {
-        // let abs: f32 = self.x * self.x + self.y * self.y + self.z * self.z;
-        // let abs = abs.sqrt() / 10.;
-        // Point3::new(self.x / abs, self.y / abs, self.z / abs)
-
+        // weird normalize function that i yanked
         let x2: f32 = self.x * self.x;
         let y2: f32 = self.y * self.y;
         let z2: f32 = self.z * self.z;
@@ -25,7 +25,6 @@ impl Normalize for Point3<f32> {
         let x: f32 = self.x * ((1.0 - (y2 + z2) / 2.0 + (y2 * z2) / 3.).sqrt());
         let y: f32 = self.y * ((1.0 - (z2 + x2) / 2.0 + (x2 * z2) / 3.).sqrt());
         let z: f32 = self.z * ((1.0 - (x2 + y2) / 2.0 + (x2 * y2) / 3.).sqrt());
-        // println!("({}, {}, {}) -> {}, {}, {}",self.x, self.y, self.z, x,y,z);
         Point3::new(x, y, z)
     }
 
@@ -34,7 +33,9 @@ impl Normalize for Point3<f32> {
     }
 }
 
-fn make_point<F: Fn(Point3<f32>) -> f32>(i: f32, j: f32, side: u8, height: &F) -> Point3<f32> {
+fn make_point<F>(i: f32, j: f32, side: u8, height: &F) -> Point3<f32> 
+    where F: Fn(Point3<f32>) -> f32 {
+    // i like where syntax
     let out = match side {
         0 => Point3::new(i, j, RADIUS),  // front
         1 => Point3::new(i, j, -RADIUS), // back
@@ -49,7 +50,11 @@ fn make_point<F: Fn(Point3<f32>) -> f32>(i: f32, j: f32, side: u8, height: &F) -
     out.scalar_mult(height)
 }
 
+/// generate mesh function
 pub fn gen_mesh<F: Fn(Point3<f32>) -> f32>(height: F) -> Rc<RefCell<Mesh>> {
+    /*!
+     * this function should, given a function that calculates at a given point on the unit sphere, return a mesh of the globe
+     */
     let mut vertices = vec![];
     let mut indices = vec![];
     let c = (2. * RADIUS) / (LEN as f32);
