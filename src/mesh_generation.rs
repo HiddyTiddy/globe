f8c0abb00a7866ccf75ec038325e231fec333d43
HiddyTiddy <hiddy.tiddey@gmail.com>
2021-11-21 19:09:08 +0100

    optimized mesh generation

--- src/mesh_generation.rs
+++ src/mesh_generation.rs
@@ -34,8 +34,10 @@ impl Normalize for Point3<f32> {
     }
 }
 
-fn make_point<F>(i: f32, j: f32, side: u8, height: &F) -> Point3<f32> 
-    where F: Fn(Point3<f32>) -> f32 {
+fn make_point<F>(i: f32, j: f32, side: u8, height: &F) -> Point3<f32>
+where
+    F: Fn(Point3<f32>) -> f32,
+{
     // i like where syntax
     let out = match side {
         0 => Point3::new(i, j, RADIUS),  // front
@@ -47,46 +49,54 @@ fn make_point<F>(i: f32, j: f32, side: u8, height: &F) -> Point3<f32>
         _ => unreachable!(),
     }
     .normalize();
-    let height = height(out);
-    out.scalar_mult(height)
+    let h = height(out);
+    out.scalar_mult(h)
 }
 
 /// generate mesh function
-pub fn gen_mesh<F: Fn(Point3<f32>) -> f32>(height: F) -> Rc<RefCell<Mesh>> {
+pub fn gen_mesh<F: Fn(Point3<f32>) -> f32>(height: F, side: u8) -> Rc<RefCell<Mesh>> {
     /*!
      * this function should, given a function that calculates at a given point on the unit sphere, return a mesh of the globe
      */
     let mut vertices = vec![];
     let mut indices = vec![];
     let c = (2. * RADIUS) / (LEN as f32);
-    let mut index = 0;
-    for side in 0..6u8 {
-        for i in 0..LEN {
-            for j in 0..LEN {
-                let y: f32 = j as f32 * c - 1.;
-                let x: f32 = i as f32 * c - 1.;
-                // not optimal
-                // redeclare like most of the vertices but it's fine
-                vertices.push(make_point(x, y, side, &height));
-                vertices.push(make_point(x + c, y, side, &height));
-                vertices.push(make_point(x, y + c, side, &height));
-                vertices.push(make_point(x + c, y + c, side, &height));
+    let mut index: u16 = 0;
+    for j in 0..LEN {
+        let x: f32 = j as f32 * c - 1.0;
+        let y: f32 = -1.0;
+        vertices.push(make_point(x, y, side, &height));
+        vertices.push(make_point(x + c, y, side, &height));
+        index += 2;
+    }
+
+    for i in 0..LEN {
+        for j in 0..LEN {
+            let x: f32 = j as f32 * c - 1.;
+            let y: f32 = i as f32 * c - 1.;
+            // better
+            vertices.push(make_point(x, y + c, side, &height));
+            vertices.push(make_point(x + c, y + c, side, &height));
+            // side == 1 | 2 | 5
+            if side == 1 || side == 2 || side == 5 {
+                indices.push(Point3::new(index, index + 1, index - (2 * LEN) as u16 + 1));
                 indices.push(Point3::new(
-                    index as u16,
-                    (index + 1) as u16,
-                    (index + 2) as u16,
+                    index,
+                    index - (2 * LEN) as u16 + 1,
+                    index - (2 * LEN) as u16,
                 ));
+            } else {
+                indices.push(Point3::new(index - (2 * LEN) as u16 + 1, index + 1, index));
                 indices.push(Point3::new(
-                    (index + 1) as u16,
-                    (index + 2) as u16,
-                    (index + 3) as u16,
+                    index - (2 * LEN) as u16,
+                    index - (2 * LEN) as u16 + 1,
+                    index,
                 ));
-                index += 4;
             }
+            index += 2;
         }
     }
     // println!("{:?} {:?}", indices.len(), vertices.len());
-
     Rc::new(RefCell::new(Mesh::new(
         vertices, indices, None, None, false,
     )))
